f47ddebb6f6a58c29473d117a70c2476c3803a98
Kennedy Francisco <kennedy24software@gmail.com>
2026-03-07 10:10:45 -0300

    feat(restaurante): adiciona controller para criar restaurante

--- src/controller/restaurante.controller.js
+++ src/controller/restaurante.controller.js
@@ -1,10 +1,21 @@
 import Restaurante from "../model/restaurante.model.js";
 
 class RestauranteController {
-  async listaRestaurantes(req, res) {
+  async listarRestaurantes(req, res) {
     try {
       const restaurantes = await Restaurante.find();
       res.status(200).json(restaurantes);
+    } catch (erro) {
+      res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  }
+
+  async criarRestaurante(req, res) {
+    try {
+      const { nome, cidade, estado, tipoCozinha, telefone } = req.body;
+      const novoRestaurante = new Restaurante({ nome, cidade, estado, tipoCozinha, telefone });
+      const restauranteSalvo = await novoRestaurante.save();
+      res.status(201).json(restauranteSalvo);
     } catch (erro) {
       res.status(500).json({ erro: "Erro interno do servidor" });
     }
