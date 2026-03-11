70d19cad9b6455dfdf0c96c76b0325546f9839a3
Kennedy Francisco <kennedy24software@gmail.com>
2026-03-10 21:20:12 -0300

    feat(app): conecta rotas com o app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import routes from "./router/index.js";
 
 const app = express();
+
+routes(app);
 
 export default app;
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import restauranteRoutes from "./restaurante.route.js";
+import pratoRoutes from "./prato.route.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send({ titulo: "API Restaurante e Pratos" });
   });
 
-  app.use(express.json());
+  app.use(express.json(), restauranteRoutes, pratoRoutes);
 };
+
+export default routes;
+
